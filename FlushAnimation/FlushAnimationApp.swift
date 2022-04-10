bfe549b920788f735a2c0c9986983fffbc926bb2
yogox <yogoyogox@gmail.com>
2022-04-10 15:49:09 +0900

    Initial Commit

---
+++ FlushAnimation/FlushAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlushAnimationApp.swift
+//  FlushAnimation
+//
+//  Created by yogox on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct FlushAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
